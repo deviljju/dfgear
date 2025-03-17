5cb210e270a6cfbde17a26b51510bbcab40439ed
deviljju <hn03049@naver.com>
2025-03-18 03:52:47 +0900

    Feat: 운없찐랭킹 추가

--- static/js/gridClass.js
+++ static/js/gridClass.js
@@ -30,23 +30,39 @@ class channelName {
   getElement() {
     return this.el;
   }
-
-//   render(props) {
-//     const propId = `${props.columnInfo.name}${props.rowKey}`;
-//     let value = props.value;
-//     let img = props.grid.store.data.rawData[props.rowKey].imgUrl;
-//     let chImg = document.getElementById(`img_${propId}`);      
-//     if(channelMap[value]){
-//       chImg.style = 'display: ;'
-//       chImg.setAttribute('src', channelMap[value]);
-//     } else if(img && props.grid.store.data.rawData[props.rowKey].stmName == value){
-//       chImg.style = 'display: ;'
-//       chImg.setAttribute('src', img);
-//     } else {
-//       chImg.style = 'display: none;'
-//     }
-//     this.el.value = String(value);
-//   }
+}
+class characterIcon {
+  constructor(props) {
+    let value = props.value;
+    const propId = `${props.columnInfo.name}${props.rowKey}`;
+    let cId = props.grid.store.data.rawData[props.rowKey].cId;
+    let sId = props.grid.store.data.rawData[props.rowKey].sId;
+    let jc = props.grid.store.data.rawData[props.rowKey].jobCss;
+		const el = document.createElement('div');
+    const el1 = document.createElement('div');
+    const el2 = document.createElement('div');
+    const chIcon = document.createElement('div');
+    el.className = 'flex flex-row gap-2';
+    el1.className = 'g-chdiv';
+    el2.className = 'g-chName g-hover-text';
+    chIcon.id = `img_${propId}`;
+    chIcon.className = `g-chimg smallIcon ${jc}`;
+    if(characterMap[value]){
+      chIcon.style = `display: ; background-image: url('${characterMap[value]}?zoom=1')`;
+    } else if(cId && props.grid.store.data.rawData[props.rowKey].characterName == value){
+      chIcon.style = `display: ; background-image: url('https://img-api.neople.co.kr/df/servers/${sId}/characters/${cId}?zoom=1')`;
+    } else {
+      chIcon.style = 'display: none;'
+    }
+    el1.append(chIcon);
+    el2.innerText = value;
+		el.appendChild(el1);
+    el.appendChild(el2);
+    this.el = el;
+	}
+  getElement() {
+    return this.el;
+  }
 }
 class colHover {
 	constructor(props) {
