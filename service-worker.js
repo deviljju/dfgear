522b0255df113fe8f3dc5db6ead98859bfdb9db6
deviljju <hn03049@naver.com>
2026-04-18 00:50:03 +0900

    fix(character): 🔨 모험단 누락 레이아웃 수정

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dfgear-cache-v4.17.1';
+const CACHE_NAME = 'dfgear-cache-v4.18';
 const FONT_CACHE_NAME = 'dfgear-fonts'; // 폰트 전용 영구 캐시 (CACHE_NAME 변경과 무관)
 
 const CACHE_LIMIT = 80; // 최대 캐시 항목 수
